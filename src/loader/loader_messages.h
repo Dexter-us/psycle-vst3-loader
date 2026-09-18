a83afc09980ca3cfa56b1e4e93e2eeb42ea195b3
Dexter U.S. <47005152+Dexter-us@users.noreply.github.com>
2026-09-17 20:00:55 -0800

    Add src/loader/loader_messages.h

---
+++ src/loader/loader_messages.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace psycle::loader {
+
+inline constexpr char kMachinePathMessageId[] = "PsycleMachinePath";
+inline constexpr char kMachineStatusMessageId[] = "PsycleMachineStatus";
+inline constexpr char kMachinePathAttributeId[] = "Path";
+inline constexpr char kMachineStatusSuccessAttributeId[] = "Success";
+inline constexpr char kMachineStatusTextAttributeId[] = "Status";
+
+} // namespace psycle::loader
